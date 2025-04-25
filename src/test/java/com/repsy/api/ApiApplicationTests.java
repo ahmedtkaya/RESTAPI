f2b0fa6b258c55c9f60976035c19ed756cb06188
Ahmed tayyib Kaya <ahmedtkaya@Ahmed-MBP.hgw.local>
2025-04-25 10:24:11 +0300

    created

---
+++ src/test/java/com/repsy/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repsy.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
